383bf6562ea77aeb75bf6bb0dc8a4fe6f85cd1d6
NathanLPS <nathan_lopes147@hotmail.com>
2024-01-04 11:06:47 -0300

    CRUD Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,45 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name ="tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 50)
+	@Size(min = 1, max = 100, message = "A categoria não pode ficar em branco")
+	@NotBlank
+	private String descricao;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
